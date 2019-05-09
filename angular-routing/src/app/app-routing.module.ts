c18317309085d2479dd763262d13dc67a57ec349
Monicka Ruthrakumaran <monicka.rk@slenix.com>
2019-05-09 17:01:41 +0530

    Added Wildcart and redirect routing

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmpListComponent } from './emp-list/emp-list.component';
 import { DeptListComponent } from './dept-list/dept-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dept-list', pathMatch: 'full' },
   { path: 'dept-list', component: DeptListComponent },
-  { path: 'emp-list', component: EmpListComponent }
+  { path: 'emp-list', component: EmpListComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -14,5 +17,5 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const RoutingConstant = [
-    DeptListComponent, EmpListComponent
+    DeptListComponent, EmpListComponent, PageNotFoundComponent
   ];
--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, RoutingConstant }
 from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingConstant
+    RoutingConstant,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
